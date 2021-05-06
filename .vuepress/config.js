3f7b27ba771aeb84bd9ba25b146aa4d54a4c63c3
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2021-05-06 23:28:52 +0800

    init-2

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "title": "xigua-web",
+  "title": "xigua web",
   "description": "xiao xigua web",
   "dest": "public",
   "head": [
@@ -15,6 +15,41 @@ module.exports = {
       {
         "name": "viewport",
         "content": "width=device-width,initial-scale=1,user-scalable=no"
+      }
+    ],
+    [
+      "meta",
+      {
+        "property": "og:title",
+        "content": "xigua web"
+      }
+    ],
+    [
+      "meta",
+      {
+        "property": "og:url",
+        "content": "https://xiaoxigua-1.github.io/public"
+      }
+    ],
+    [
+      "meta",
+      {
+        "property": "og:image",
+        "content": "https://cdn.discordapp.com/avatars/458988300418416640/32b31c8b5dd7b3418b4bd9f2f2f11594.png?size=4096"
+      }
+    ],
+    [
+      "meta",
+      {
+        "property": "og:description",
+        "content": "xiao xigua web"
+      }
+    ],
+    [
+      "meta",
+      {
+        "property": "og:site_name",
+        "content": "xigua web"
       }
     ]
   ],
@@ -32,35 +67,17 @@ module.exports = {
         "icon": "reco-date"
       },
       {
-        "text": "Docs",
-        "icon": "reco-message",
-        "items": [
-          {
-            "text": "vuepress-reco",
-            "link": "/docs/theme-reco/"
-          }
-        ]
-      },
-      {
-        "text": "Contact",
+        "text": "GitHub",
         "icon": "reco-message",
         "items": [
           {
             "text": "GitHub",
-            "link": "https://github.com/recoluan",
+            "link": "https://github.com/xiaoxigua-1/xiaoxigua-1.github.io",
             "icon": "reco-github"
           }
         ]
       }
     ],
-    "sidebar": {
-      "/docs/theme-reco/": [
-        "",
-        "theme",
-        "plugin",
-        "api"
-      ]
-    },
     "type": "blog",
     "blogConfig": {
       "category": {
@@ -74,26 +91,26 @@ module.exports = {
     },
     "friendLink": [
       {
-        "title": "午后南杂",
-        "desc": "Enjoy when you can, and endure when you must.",
-        "email": "1156743527@qq.com",
-        "link": "https://www.recoluan.com"
+        "title": "GitHub",
+        "desc": "My GitHub URL",
+        "email": "aijdfajodwsdf@gmail.com",
+        "link": "https://github.com/xiaoxigua-1"
       },
       {
-        "title": "vuepress-theme-reco",
-        "desc": "A simple and beautiful vuepress Blog & Doc theme.",
-        "avatar": "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
-        "link": "https://vuepress-theme-reco.recoluan.com"
+        "title": "Xiao xigua web",
+        "desc": "My web GitHub repo URL",
+        "avatar": "https://cdn.discordapp.com/avatars/458988300418416640/32b31c8b5dd7b3418b4bd9f2f2f11594.png?size=4096",
+        "link": "https://github.com/xiaoxigua-1/xiaoxigua-1.github.io"
       }
     ],
-    "logo": "/logo.png",
+    "logo": "https://cdn.discordapp.com/avatars/458988300418416640/32b31c8b5dd7b3418b4bd9f2f2f11594.png?size=4096",
     "search": true,
     "searchMaxSuggestions": 10,
     "lastUpdated": "Last Updated",
     "author": "xiao xigua",
-    "authorAvatar": "/avatar.png",
+    "authorAvatar": "https://cdn.discordapp.com/avatars/458988300418416640/32b31c8b5dd7b3418b4bd9f2f2f11594.png?size=4096",
     "record": "xxxx",
-    "startYear": "2017"
+    "startYear": "2021"
   },
   "markdown": {
     "lineNumbers": true
--- docs/theme-reco/README.md
+++ docs/theme-reco/README.md
@@ -1,6 +1,13 @@
 ---
-title: theme-reco
-date: 2020-05-29
+title: 個人網站
+date: 2021-05-06
+categories:
+ - frontEnd
+tags:
+ - 廢文
 ---
 
-This is theme-reco.
+我自己的第一個個人網站ww
+而這篇則是第一篇文章
+由於我自己懶的寫所以使用
+現成VuePress theme
